ca9cbef63bc04bb49ef21a1a7ea107ae92ada879
Yuberley <yuberleyguerrero1999@gmail.com>
2021-02-04 00:57:46 -0500

    Creacion de app posts

--- Photogram/urls.py
+++ Photogram/urls.py
@@ -1,14 +1,17 @@
 """Module for Photogram"""
 
 from django.urls import path
-from Photogram import views
+from Photogram import views as local_views
+from posts import views as posts_views
 
 def hello_world(requiest):
     """Return a greeting"""
     return HttpResponse('Hello, World')
 
 urlpatterns = [
-    path('hello_world/', views.hello_world),
-    path('sorted/', views.sort_integers),
-    path('hi/<str:name>/<int:age>/', views.say_hi),
+    path('hello_world/', local_views.hello_world),
+    path('sorted/', local_views.sort_integers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+
+    path('posts/', posts_views.list_posts),
 ]
--- posts/apps.py
+++ posts/apps.py
@@ -1,5 +1,10 @@
+"""Post application module"""
+
 from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
+    """Post aplication settings."""
+
     name = 'posts'
+    varbose_name = 'Posts'
